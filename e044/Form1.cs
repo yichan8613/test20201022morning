e3470a3c2276e213f8a9d14df0edb70251b404d1
宜詮 廖 <yichan8613@gmail.com>
2020-10-22 08:08:15 +0800

    1

---
+++ e044/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace e044
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Btn1_Click_1(object sender, EventArgs e)
+        {
+            //說話
+            Console.WriteLine("HI~請你跟我這樣做");
+
+        }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+            //舉起右手
+
+            if (true)
+			{
+
+			} 
+
+
+
+
+
+
+
+            
+		}
+	}
+}
